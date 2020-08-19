20cca3c05e314ed7b0ea1d73d929538380cd4c9b
NgChun0 <987393859@qq.com>
2020-08-19 11:22:33 +0800

    windowflags and windowstate of widgets

--- mywidget1/main.cpp
+++ mywidget1/main.cpp
@@ -4,8 +4,19 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWidget *widget = new QWidget();
+    //QWidget(QWidget* parent = 0, Qt::WindowFlags f = 0)
+    //QWidget *widget = new QWidget(0, Qt::Dialog);
+    //QWidget *widget = new QWidget(0, Qt::SplashScreen);
+    //QWidget *widget = new QWidget(0, Qt::Dialog | Qt::FramelessWindowHint);
+    //QWidget *widget = new QWidget(0, Qt::SplashScreen | Qt::WindowStaysOnTopHint);
+    //QWidget *widget = new QWidget(0, Qt::Desktop);
 
     widget->setWindowTitle(QObject::tr("我是widget"));
+    //widget->setWindowState(Qt::WindowMaximized);
+    //widget->setWindowState(Qt::WindowMinimized);
+    //widget->setWindowState(Qt::WindowFullScreen);
+    //widget->setWindowState(Qt::WindowActive);
+    widget->setWindowState(Qt::WindowNoState);
 
     QLabel *label = new QLabel();
     label->setWindowTitle(QObject::tr("我是label"));
